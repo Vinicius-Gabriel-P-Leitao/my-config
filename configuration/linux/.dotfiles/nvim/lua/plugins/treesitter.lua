32db8523bbe44a38a7475f496aa37254e1e92b07
Vinicius-Gabriel-P-Leitao <vinicius.gabriel.p.leitao@gmail.com>
2025-05-20 18:28:22 -0300

    [feat]: Altera configurações do .dotfiles.
    Nvim
    Kitty

--- configuration/linux/.dotfiles/nvim/lua/plugins/treesitter.lua
+++ configuration/linux/.dotfiles/nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,11 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- NOTE: REMOVE THIS LINE TO INACTIVATE THIS FILE
 
 -- Customize Treesitter
 
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
+
   opts = {
     ensure_installed = {
       "lua",
