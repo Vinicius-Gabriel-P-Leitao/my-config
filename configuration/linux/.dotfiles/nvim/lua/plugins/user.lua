0ad83463ca26b5866a2746cee4d51d39ac4ec548
Vinicius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2025-06-04 05:09:45 -0300

    [feat]: Adiciona thema actppuccina ao astrovim

--- configuration/linux/.dotfiles/nvim/lua/plugins/user.lua
+++ configuration/linux/.dotfiles/nvim/lua/plugins/user.lua
@@ -68,4 +68,26 @@ return {
       }, Rule("a", "a", "-vim"))
     end,
   },
+
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup {
+        flavour = "mocha",
+        transparent_background = true,
+        no_italic = false,
+        integrations = {
+          treesitter = true,
+          lsp_trouble = true,
+          cmp = true,
+          nvimtree = true,
+          telescope = true,
+        },
+      }
+
+      vim.cmd.colorscheme "catppuccin"
+    end,
+  },
 }
