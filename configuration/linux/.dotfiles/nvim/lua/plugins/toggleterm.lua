640bc108366ea6d7f033ba86a24324a47124ed9e
Vinicius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2025-06-15 09:53:53 -0300

    [feat]: Toggleterm agora funciona via pwsh do windows ou fish no linux.

--- configuration/linux/.dotfiles/nvim/lua/plugins/toggleterm.lua
+++ configuration/linux/.dotfiles/nvim/lua/plugins/toggleterm.lua
@@ -1,7 +1,11 @@
 return {
   "akinsho/toggleterm.nvim",
-  opts = {
-    shell = "pwsh",
-    direction = "horizontal",
-  },
+  opts = function()
+    local shell = vim.fn.has("win32") == 1 and "pwsh" or "fish"
+
+    return {
+      shell = shell,
+      direction = "horizontal",
+    }
+  end,
 }
