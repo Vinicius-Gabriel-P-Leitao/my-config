234f877919c17cb64556dc39866a83aaef0a8f9b
Vinicius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2025-06-15 09:04:38 -0300

    [feat]: Novas configurações para o lazyvim.
    Configuração de lsp, keymaps e do neo-tree

--- configuration/linux/.dotfiles/nvim/lua/config/keymaps.lua
+++ configuration/linux/.dotfiles/nvim/lua/config/keymaps.lua
@@ -1,4 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- PowerShell como terminal
 vim.keymap.set("n", "<leader>ft", "<cmd>ToggleTerm<CR>", { desc = "Abrir PowerShell" })
+
+-- Ctrl+C copia a seleção (equivalente ao 'y' visual)
+vim.api.nvim_set_keymap("v", "<C-c>", "y", { noremap = true, silent = true })
+
+-- Ctrl+V cola no modo normal (já mapeado acima), mas se quiser colar no visual:
+vim.api.nvim_set_keymap("v", "<C-v>", "p", { noremap = true, silent = true })
