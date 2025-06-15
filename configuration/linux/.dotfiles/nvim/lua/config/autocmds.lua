9cee8e0edba853e420c7887f75dc037b9fd13d5d
Vinicius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2025-06-15 19:11:57 -0300

    [refactor]: Remove configurações manuais e deixa o lazyvim fazer o show.

--- configuration/linux/.dotfiles/nvim/lua/config/autocmds.lua
+++ configuration/linux/.dotfiles/nvim/lua/config/autocmds.lua
@@ -7,32 +7,9 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
-vim.api.nvim_create_autocmd("BufEnter", {
-  group = vim.api.nvim_create_augroup("Neotree_start_directory", { clear = true }),
-  desc = "Start Neo-tree with directory",
-  once = true,
+-- Configura autocmd para rodar linting ao salvar
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
   callback = function()
-    if package.loaded["neo-tree"] then
-      return
-    end
-
-    local argv = vim.fn.argv()
-    if type(argv) == "string" then
-      argv = { argv }
-    end
-
-    if not argv or vim.tbl_isempty(argv) then
-      return
-    end
-
-    local path = argv[1]
-    if type(path) ~= "string" or path == "" then
-      return
-    end
-
-    local stats = vim.uv.fs_stat(path)
-    if stats and stats.type == "directory" then
-      require("neo-tree")
-    end
+    require("lint").try_lint()
   end,
 })
