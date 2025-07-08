8ccb8456b76dd3aaf9b189b510ca14f9ba9a8a35
Vinicius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2025-07-08 09:51:03 -0300

    [feat]: Hyprland configuration dotfiles.

---
+++ configuration/linux/.dotfiles/hypr/script/refresh_waybar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Check if waybar is running
+if pgrep -x "waybar" > /dev/null; then
+    # If running, kill the waybar process
+    killall "waybar"
+else
+    # If not running, start waybar
+    waybar &
+fi
