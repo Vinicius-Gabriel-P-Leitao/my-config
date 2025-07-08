63a78ca182800fc6e3ab8bc39e3444a8bb67f5b6
Vinicius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2025-07-08 20:56:53 -0300

    [feat]: Remove script  e adiciona o keychain para carregar o agente ssh.

--- configuration/linux/.dotfiles/hypr/script/start_ssh_agent.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-    ssh-agent -s > ssh-agent -s | grep -v 'echo Agent pid' > "$HOME/.ssh-agent-env"
-fi
-
-# In .bashrc
-source "$HOME/.ssh-agent-env"
