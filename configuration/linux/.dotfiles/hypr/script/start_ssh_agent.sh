b8eea4c6e29e724c387a44403d0d444b8719371d
Vinicius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2025-07-09 01:16:18 -0300

    [fixed]: Corrige script para poder funcionar no fish.

--- configuration/linux/.dotfiles/hypr/script/start_ssh_agent.sh
+++ configuration/linux/.dotfiles/hypr/script/start_ssh_agent.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-# Caminho do binário askpass que você instalou
 export SSH_ASKPASS=/usr/bin/ksshaskpass
-export DISPLAY=:0 # necessário pra GUI funcionar
+export DISPLAY=:0
 
-# Inicia ssh-agent
-eval $(ssh-agent -s)
+SSH_ENV="$HOME/.ssh-agent-env"
 
-# Força uso do askpass mesmo sem terminal
-ssh-add ~/.ssh/id_ed25519 </dev/null
+if [ -f "$SSH_ENV" ]; then
+  source "$SSH_ENV" >/dev/null
+
+  if ! kill -0 "$SSH_AGENT_PID" 2>/dev/null; then
+    echo "Agente morto, iniciando novo..."
+    rm -f "$SSH_ENV"
+    exec $0
+  fi
+else
+  eval $(ssh-agent -s) >/dev/null
+  echo "export SSH_AUTH_SOCK=$SSH_AUTH_SOCK" >"$SSH_ENV"
+  echo "export SSH_AGENT_PID=$SSH_AGENT_PID" >>"$SSH_ENV"
+fi
+
+DISPLAY=:0 SSH_ASKPASS=/usr/bin/ksshaskpass setsid ssh-add ~/.ssh/id_ed25519 </dev/null
