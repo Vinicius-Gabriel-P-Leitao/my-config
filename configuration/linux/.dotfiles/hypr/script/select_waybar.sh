3aa564b07443412e7e099178fd03c4ae62d6a1c6
Vinicius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2025-07-24 03:00:44 -0300

    [feat]: Selecionar wallpaper mata processo do quickshell e inicia novamente

--- configuration/linux/.dotfiles/hypr/script/select_waybar.sh
+++ configuration/linux/.dotfiles/hypr/script/select_waybar.sh
@@ -7,37 +7,37 @@ ASSETS="$WAYBAR_DIR/assets"
 THEMES="$WAYBAR_DIR/themes"
 
 menu() {
-    find "${ASSETS}" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) | awk '{print "img:"$0}'
+  find "${ASSETS}" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) | awk '{print "img:"$0}'
 }
 
 main() {
-    pkill wofi
-    choice=$(menu | wofi -c ~/.config/wofi/custom/waybar -s ~/.config/wofi/custom/style-waybar.css --show dmenu --prompt " Select Waybar (Scroll with Arrows)" -n)
+  pkill wofi
+  choice=$(menu | wofi -c ~/.config/wofi/custom/waybar -s ~/.config/wofi/custom/style-waybar.css --show dmenu --prompt " Select Waybar (Scroll with Arrows)" -n)
 
-    selected_waybar="${choice#img:}"
-    [ -z "$selected_waybar" ] && exit 1
-    echo $selected_waybar
+  selected_waybar="${choice#img:}"
+  [ -z "$selected_waybar" ] && exit 1
+  echo $selected_waybar
 
-    if [[ "$selected_waybar" == "$ASSETS/experimental.png" ]]; then
-        cat $THEMES/experimental/style-experimental.css >$STYLECSS
-        cat $THEMES/experimental/config-experimental.jsonc >$CONFIG
-        pkill waybar && waybar
+  if [[ "$selected_waybar" == "$ASSETS/experimental.png" ]]; then
+    cat $THEMES/experimental/style-experimental.css >$STYLECSS
+    cat $THEMES/experimental/config-experimental.jsonc >$CONFIG
+    pkill waybar && waybar
 
-    elif [[ "$selected_waybar" == "$ASSETS/main.png" ]]; then
-        cat $THEMES/default/style-default.css >$STYLECSS
-        cat $THEMES/default/config-default.jsonc >$CONFIG
-        pkill waybar && waybar
+  elif [[ "$selected_waybar" == "$ASSETS/main.png" ]]; then
+    cat $THEMES/default/style-default.css >$STYLECSS
+    cat $THEMES/default/config-default.jsonc >$CONFIG
+    pkill waybar && waybar
 
-    elif [[ "$selected_waybar" == "$ASSETS/line.png" ]]; then
-        cat $THEMES/line/style-line.css >$STYLECSS
-        cat $THEMES/line/config-line.jsonc >$CONFIG
-        pkill waybar && waybar
+  elif [[ "$selected_waybar" == "$ASSETS/line.png" ]]; then
+    cat $THEMES/line/style-line.css >$STYLECSS
+    cat $THEMES/line/config-line.jsonc >$CONFIG
+    pkill waybar && waybar
 
-    elif [[ "$selected_waybar" == "$ASSETS/zen.png" ]]; then
-        cat $THEMES/zen/style-zen.css >$STYLECSS
-        cat $THEMES/zen/config-zen.jsonc >$CONFIG
-        pkill waybar && waybar
-    fi
+  elif [[ "$selected_waybar" == "$ASSETS/zen.png" ]]; then
+    cat $THEMES/zen/style-zen.css >$STYLECSS
+    cat $THEMES/zen/config-zen.jsonc >$CONFIG
+    pkill waybar && waybar
+  fi
 }
 
 main
