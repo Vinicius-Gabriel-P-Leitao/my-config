772408b4535d6bd11309587ab97be2090c065766
Vinicius-Gabriel-P-Leitao <vinicius.gabriel.p.leitao@gmail.com>
2025-12-03 12:18:12 -0300

    [feat]: Configuração do oh-my-posh e atualização do wezterm para windows.

--- configuration/windows/.config/.wezterm.lua
+++ configuration/windows/.config/.wezterm.lua
@@ -1,22 +1,19 @@
 local wezterm = require("wezterm")
 local platform = wezterm.target_triple
 
-local default = {
-    color_scheme = "Catppuccin Mocha",
+local config = {
+	color_scheme = "Catppuccin Mocha",
 }
 
-local shells = {
-    windows = { "pwsh.exe", "-NoLogo" },
-    linux   = { "/usr/bin/fish", "-l" },
-    darwin  = { "/opt/homebrew/bin/fish", "-l" },
-}
-
-for key, cmd in pairs(shells) do
-    if platform:find(key) then
-        default.default_prog = cmd
-        return default
-    end
+if platform:find("linux") then
+	config.default_prog = { "/usr/bin/fish", "-l" }
+	config.enable_kitty_graphics = true
+	config.enable_wayland = true
 end
 
-default.default_prog = { wezterm.shell(), "-l" }
-return default
+if platform:find("windows") then
+	config.default_prog = { "pwsh.exe", "-NoLogo" }
+	config.animation_fps = 120
+end
+
+return config
