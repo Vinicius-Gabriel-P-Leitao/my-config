e1ab9abb05f13563e82c2214babd2bd343a51274
Vinicius-Gabriel-P-Leitao <vinicius.gabriel.p.leitao@gmail.com>
2025-12-09 09:43:26 -0300

    [feat]: Otimiza script do grub.

--- configuration/linux/.grub/theme/install.sh
+++ configuration/linux/.grub/theme/install.sh
@@ -1,110 +1,93 @@
 #!/bin/bash
 
-# Grub2 Theme
-# WARN: Esse script foi feito pelo projeto Elegant-grub2-themes, apenas modifiquei as imagens.
-
 ROOT_UID=0
 THEME_DIR="/usr/share/grub/themes"
 THEME_NAME=frieren-window-left-dark
 
-MAX_DELAY=20                                        # max delay for user to enter root password
+MAX_DELAY=20
 
-#COLORS
-CDEF=" \033[0m"                                     # default color
-CCIN=" \033[0;36m"                                  # info color
-CGSC=" \033[0;32m"                                  # success color
-CRER=" \033[0;31m"                                  # error color
-CWAR=" \033[0;33m"                                  # waring color
-b_CDEF=" \033[1;37m"                                # bold default color
-b_CCIN=" \033[1;36m"                                # bold info color
-b_CGSC=" \033[1;32m"                                # bold success color
-b_CRER=" \033[1;31m"                                # bold error color
-b_CWAR=" \033[1;33m"                                # bold warning color
+CDEF=" \033[0m"
+b_CCIN=" \033[1;36m"
+b_CGSC=" \033[1;32m"
+b_CRER=" \033[1;31m"
+b_CWAR=" \033[1;33m"
 
-# echo like ...  with  flag type  and display message  colors
-prompt () {
+# Função de mandar as msg bonitinhas
+prompt() {
   case ${1} in
-    "-s"|"--success")
-      echo -e "${b_CGSC}${@/-s/}${CDEF}";;          # print success message
-    "-e"|"--error")
-      echo -e "${b_CRER}${@/-e/}${CDEF}";;          # print error message
-    "-w"|"--warning")
-      echo -e "${b_CWAR}${@/-w/}${CDEF}";;          # print warning message
-    "-i"|"--info")
-      echo -e "${b_CCIN}${@/-i/}${CDEF}";;          # print info message
-    *)
+  "-s" | "--success")
+    echo -e "${b_CGSC}${*//-s/}${CDEF}"
+    ;;
+  "-e" | "--error")
+    echo -e "${b_CRER}${*//-e/}${CDEF}"
+    ;;
+  "-w" | "--warning")
+    echo -e "${b_CWAR}${*//-w/}${CDEF}"
+    ;;
+  "-i" | "--info")
+    echo -e "${b_CCIN}${*//-i/}${CDEF}"
+    ;;
+  *)
     echo -e "$@"
     ;;
   esac
 }
 
-# Welcome message
-prompt -s "\n ${THEME_NAME} - Grub2 Theme \n"
-
-# Check command avalibility
 function has_command() {
-  command -v $1 > /dev/null
+  command -v -- "$1 >/dev/null"
 }
 
-prompt -w "Checking for root access...\n"
+prompt -w "Verificando se foi executado com root...\n"
 
-# Checking for root access and proceed if it is present
 if [ "$UID" -eq "$ROOT_UID" ]; then
 
-  # Create themes directory if not exists
-  prompt -i "Checking for the existence of themes directory...\n"
-  [[ -d ${THEME_DIR}/${THEME_NAME} ]] && rm -rf ${THEME_DIR}/${THEME_NAME}
+  # Cria a pasta do thema
+  prompt -i "Verificando se a pasta do thema existe...\n"
+  [[ -d ${THEME_DIR:?}/${THEME_NAME} ]] && rm -rf "${THEME_DIR:?}/${THEME_NAME}"
   mkdir -p "${THEME_DIR}/${THEME_NAME}"
 
-  # Copy theme
-  prompt -i "Installing ${THEME_NAME} theme...\n"
-
+  # Copia o thema para pasta do grub
+  prompt -i "Instalando o thema ${THEME_NAME}...\n"
   cp -a ${THEME_NAME}/* ${THEME_DIR}/${THEME_NAME}
 
-  # Set theme
-  prompt -i "Setting ${THEME_NAME} as default...\n"
-
-  # Backup grub config
+  prompt -i "Coloca o thema no arquivo do grub...\n"
   cp -an /etc/default/grub /etc/default/grub.bak
+  grep "GRUB_THEME=" /etc/default/grub >/dev/null 2>&1 && sed -i '/GRUB_THEME=/d' /etc/default/grub
+  echo "GRUB_THEME=\"${THEME_DIR}/${THEME_NAME}/theme.txt\"" >>/etc/default/grub
 
-  grep "GRUB_THEME=" /etc/default/grub 2>&1 >/dev/null && sed -i '/GRUB_THEME=/d' /etc/default/grub
-
-  echo "GRUB_THEME=\"${THEME_DIR}/${THEME_NAME}/theme.txt\"" >> /etc/default/grub
-
-  # Update grub config
-  prompt -i "Updating grub config...\n"
+  # Atualiza a config do grub grub-mkconfig
+  prompt -i "Grub config...\n"
   if has_command update-grub; then
     update-grub
   elif has_command grub-mkconfig; then
     grub-mkconfig -o /boot/grub/grub.cfg
-  # Check for OpenSuse (regular or microOS)
   elif has_command zypper || has_command transactional-update; then
     grub2-mkconfig -o /boot/grub2/grub.cfg
-  # Check for Fedora (regular or Atomic)
   elif has_command dnf || has_command rpm-ostree; then
-    # Check for UEFI
+
+    # Verifica se o EFI do grub tá disponivel no fedora ou demais distros
+    # WARN: Fedora tem o grub instalado diferente
     if [[ -f /boot/efi/EFI/fedora/grub.cfg ]]; then
-      prompt -s "Find config file on /boot/efi/EFI/fedora/grub.cfg ...\n"
+      prompt -s "Procurando config do grub /boot/efi/EFI/fedora/grub.cfg...\n"
       grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
-    # Check for BIOS
+
     elif [[ -f /boot/grub2/grub.cfg ]]; then
-      prompt -s "Find config file on /boot/grub2/grub.cfg ...\n"
+      prompt -s "Procurando config do grub boot/grub2/grub.cfg...\n"
       grub2-mkconfig -o /boot/grub2/grub.cfg
     fi
   fi
 
-  # Success message
-  prompt -s "\n All done! \n"
+  prompt -s "\n Instalado! \n"
 else
-  # Error message
-  prompt -e "[ Error! ] -> Run me as root "
+  prompt -e "[ Error! ] -> Necessário de root "
 
-  # persisted execution of the script as root
-  read -p "[ trusted ] specify the root password : " -t ${MAX_DELAY} -s
-  [[ -n "$REPLY" ]] && {
-    sudo -S <<< $REPLY $0
-  } || {
-    prompt  "Operation canceled  Bye"
+  read -r -p "[ trusted ] Coloque a senha de root: " -t "${MAX_DELAY}" -s
+
+  if [[ -n "$REPLY" ]]; then
+    sudo -S "$0" <<<"$REPLY"
+  else
+    prompt "Cancelado"
     exit 1
-  }
+  fi
+
 fi
